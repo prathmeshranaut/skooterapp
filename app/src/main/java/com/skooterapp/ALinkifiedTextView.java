bbe601864e330b55f24cc39d60039f13121602f9
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-03-20 16:43:12 +0530

    Clickable links in skoots and comments

---
+++ app/src/main/java/com/skooterapp/ALinkifiedTextView.java
@@ -0,0 +1,69 @@
+package com.skooterapp;
+
+import android.content.Context;
+import android.text.Layout;
+import android.text.Selection;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.style.ClickableSpan;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+/**
+ * Created by aayushranaut on 3/20/15.
+ * com.skooterapp
+ */
+public class ALinkifiedTextView extends ATextView {
+
+    public ALinkifiedTextView(Context context) {
+        super(context);
+    }
+
+    public ALinkifiedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ALinkifiedTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        TextView widget = (TextView) this;
+        Object text = widget.getText();
+        if (text instanceof Spanned) {
+            Spannable buffer = (Spannable) text;
+            int action = event.getAction();
+
+            if (action == MotionEvent.ACTION_UP
+                    || action == MotionEvent.ACTION_DOWN) {
+                int x = (int) event.getX();
+                int y  = (int) event.getY();
+                x -= widget.getTotalPaddingLeft();
+                y -= widget.getTotalPaddingTop();
+
+                x += widget.getScrollX();
+                y += widget.getScrollY();
+
+                Layout layout = widget.getLayout();
+                int line = layout.getLineForVertical(y);
+                int off = layout.getOffsetForHorizontal(line, x);
+
+                ClickableSpan[] link = buffer.getSpans(off, off, ClickableSpan.class);
+
+                if (link.length != 0) {
+                    if (action == MotionEvent.ACTION_UP) {
+                        link[0].onClick(widget);
+                    } else if (action == MotionEvent.ACTION_DOWN) {
+                        Selection.setSelection(buffer,
+                                buffer.getSpanStart(link[0]),
+                                buffer.getSpanEnd(link[0]));
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
